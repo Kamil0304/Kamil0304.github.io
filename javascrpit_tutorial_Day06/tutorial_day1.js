3f5a2695f8e49234d87b401945285e1b10453024
Kamil0304 <76685553+Kamil0304@users.noreply.github.com>
2021-11-02 20:59:40 -0500

    commit message

---
+++ javascrpit_tutorial_Day06/tutorial_day1.js
@@ -0,0 +1,40 @@
+function x(){
+var x=10;
+function fun() {
+ console.log(x);
+ x = 20;
+ if (x > 0) {
+ var x = 30;
+ console.log(x);
+ }
+ var x = 40;
+ var f = function(x) {
+ console.log(x); 
+ }
+console.log(50);
+} //run it
+
+fun();
+console.log(x);
+}
+
+x();
+
+function findMax() { 
+    var i; 
+    var max = -Infinity; 
+    for (i = 0; i < arguments.length; i++) { 
+    if (arguments[i] > max) { 
+    max = arguments[i]; 
+    } 
+    } 
+    return max; 
+    } 
+    var y = findMax(1, 123, 500, 115, 44, 88); // 500
+    var x = findMax(5, 32, 24); // 32
+    console.log(y);
+    console.log(x);
+
+    var k = (x,y)=>{ return x*y};
+    console.log(k(10,80))
+    
